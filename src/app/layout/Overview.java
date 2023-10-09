d49920acca8ba77e2c61a615302b913bbc7df2c1
Roberto Vicario <rvicariodev@gmail.com>
2023-10-09 10:24:05 +0200

    updates

--- src/app/layout/Overview.java
+++ src/app/layout/Overview.java
@@ -9,7 +9,6 @@ import java.awt.event.MouseEvent;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.Objects;
 
 /**
  * @author Roberto Vicario
